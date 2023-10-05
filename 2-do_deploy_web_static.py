136169b36cd8b4b214b8593e0d9541cc86515cb8
Anas Ribki <anasribki147@gmail.com>
2023-10-05 14:52:11 +0100

    Edit task 2. Deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,31 +17,31 @@ def do_deploy(archive_path):
     Distributes an archive to the web
     servers
     """
-    if not path.exists(archive_path):
-        return False
     try:
         """Get the file name without ext"""
+        if not path.exists(archive_path):
+            return False
         file_name = path.basename(archive_path)
         no_ext = file_name.split(".")[0]
         """Upload the archive"""
         put(archive_path, "/tmp/")
         """Uncompress the archive"""
-        run("mkdir -p /data/web_static/releases/{}".format(no_ext))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
+        run("sudo mkdir -p /data/web_static/releases/{}".format(no_ext))
+        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}"
             .format(file_name, no_ext))
         """Delete the archive"""
-        run("rm /tmp/{}".format(file_name))
+        run("sudo rm /tmp/{}".format(file_name))
         """Move contents to host web_static"""
-        run('mv /data/web_static/releases/{}/web_static/* \
+        run('sudo mv /data/web_static/releases/{}/web_static/* \
 /data/web_static/releases/{}/'.format(no_ext, no_ext))
         """Remove web_static dir"""
-        run('rm -rf /data/web_static/releases/\
+        run('sudo rm -rf /data/web_static/releases/\
 {}/web_static'.format(no_ext))
         """Delete the symbolic link"""
-        run("rm -rf /data/web_static/current")
+        run("sudo rm -rf /data/web_static/current")
         """Create a new symbolic link"""
-        run("ln -s /data/web_static/releases/{} /data/web_static/current"
+        run("sudo ln -s /data/web_static/releases/{} /data/web_static/current"
             .format(no_ext))
-        return True
     except Exception as e:
         return False
+    return True
